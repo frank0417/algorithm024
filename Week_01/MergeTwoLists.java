01da329b18fc1386c915b35a275fcb60b5d01ae6
frank0417 <frank0417@163.com>
2021-01-27 21:47:48 +0800

    add

--- Week_01/MergeTwoLists.java
+++ Week_01/MergeTwoLists.java
@@ -6,7 +6,27 @@ public class MergeTwoLists {
      * 描 述：将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        dummy.next = l1;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur = l1;
+                l1 = l1.next;
+                continue;
+            } else {
+                cur.next=l2;
+                l2=l2.next;
+                cur=cur.next;
+                cur.next=l1;
+            }
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return dummy.next;
     }
 }
 
